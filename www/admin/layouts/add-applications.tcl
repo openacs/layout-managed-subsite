6472c354a06b581e0968e1b87590b2a5bb204f84
Don Baccus <dhogaza@pacifier.com>
2010-01-08 20:35:03 +0000

    Misspelled message resource key.

--- www/admin/layouts/add-applications.tcl
+++ www/admin/layouts/add-applications.tcl
@@ -7,6 +7,6 @@ ad_page_contract {
     @cvs-id $Id$
 }
 
-set context [list "[_ layout-subsite-manager.Add_applications]"]
+set context [list "[_ layout-managed-subsite.Add_applications]"]
 set return_url [ad_conn url]?[ad_conn query]
 ad_return_template
